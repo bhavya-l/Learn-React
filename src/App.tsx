5eba74ff67664a20d539f010fac17e88b24aa7b0
Bhavya Lamba <blamba@uwaterloo.ca>
2024-05-15 21:55:37 -0400

    Components and props added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import Message from "./Message"
+import Alert from "./Components/Alert";
+import ListGroup from "./Components/ListGroup";
 
 function App() {
-  return <div><Message/></div>;
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+  return <div>
+    <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}/>
+    <Alert>
+      <span>Hello World</span>
+    </Alert>
+    </div>;
 }
-
 export default App;
